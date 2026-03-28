9b062de48daab291f25f809d64a09b3576b31f12
rmoore98-uncc <rmoore98@charlotte.edu>
2026-03-28 12:01:27 -0400

    Add files via upload

--- mvp1.2.py
+++ mvp1.2.py
@@ -211,7 +211,7 @@ def run_rag(user_query):
 
 # ✅ Use enriched docs for map/UI
     docs_for_map = enrich_with_location(docs)
-    st.write("DEBUG docs_for_map:", docs_for_map)
+    st.write("DEBUG raw docs:", docs)
 
 
     st.session_state.last_docs = docs_for_map
