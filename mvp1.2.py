78bae75abfad0d16caebc59c1f378d92818ef734
rmoore98-uncc <rmoore98@charlotte.edu>
2026-04-18 13:12:42 -0400

    Add files via upload

--- mvp1.2.py
+++ mvp1.2.py
@@ -23,7 +23,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DB_PASSWORD = os.getenv("PASSWORD")
 
-client = wrap_openai(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY)
 langsmith_client = LangSmithClient()
 
 # -----------------------------
